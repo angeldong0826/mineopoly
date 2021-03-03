a236ebb5534f7c2e9e6301296dd6b54875d6fd87
angeldong0826 <adong6@illinois.edu>
2021-03-03 00:30:17 -0600

    Added strategy to competition folder.

---
+++ src/main/java/mineopoly_three/competition/PlayerStrategy.java
@@ -0,0 +1,171 @@
+package mineopoly_three.competition;
+
+import java.awt.Point;
+import java.util.Random;
+import mineopoly_three.action.TurnAction;
+import mineopoly_three.game.Economy;
+import mineopoly_three.item.InventoryItem;
+import mineopoly_three.strategy.MinePlayerStrategy;
+import mineopoly_three.strategy.PlayerBoardView;
+import mineopoly_three.tiles.TileType;
+
+/**
+ * PlayerStrategy class that implements MinePlayerStrategy interface and contains one strategy of
+ *  winning the Mine-opoly game.
+ */
+public class PlayerStrategy implements MinePlayerStrategy {
+    private static int currentBoardSize; // length and width of the square game board
+    private int maxInventorySize; // maximum number of items that your player can carry at one time
+    private int maxCharge; // amount of charge your robot starts with (number of tile moves before needing to recharge)
+    private Economy economy; // GameEngine's economy object which holds current prices for resources
+    private Point currentLocation = new Point(); // current location of player on board
+    private Point destination = new Point(); // destination player tries to reach
+    private int currentInventorySize = 0; // current size of inventory
+    private int currentScore = 0; // current score of game
+    private int mineCount = 0; // number of times mined
+
+    public PlayerStrategy() {};
+
+    public static int getCurrentBoardSize() {
+        return currentBoardSize;
+    }
+
+    public Point getDestination() {
+        return destination;
+    }
+
+    public Economy getEconomy() {
+        return economy;
+    }
+
+    public Point getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public int getCurrentInventorySize() {
+        return currentInventorySize;
+    }
+
+    /**
+     * Initialize method that gets called on at the beginning of every game round.
+     *
+     * @param boardSize The length and width of the square game board
+     * @param maxInventorySize The maximum number of items that your player can carry at one time
+     * @param maxCharge The amount of charge your robot starts with (number of tile moves before needing to recharge)
+     * @param winningScore The first player to reach this score wins the round
+     * @param startingBoard A view of the GameBoard at the start of the game. You can use this to pre-compute fixed
+     *                       information, like the locations of market or recharge tiles
+     * @param startTileLocation A Point representing your starting location in (x, y) coordinates
+     *                              (0, 0) is the bottom left and (boardSize - 1, boardSize - 1) is the top right
+     * @param isRedPlayer True if this strategy is the red player, false otherwise
+     * @param random A random number generator, if your strategy needs random numbers you should use this.
+     */
+    @Override
+    public void initialize(int boardSize, int maxInventorySize, int maxCharge, int winningScore,
+            PlayerBoardView startingBoard, Point startTileLocation, boolean isRedPlayer, Random random) {
+        currentBoardSize = boardSize;
+        this.maxInventorySize = maxInventorySize;
+        this.maxCharge = maxCharge;
+
+        Tool tool = new Tool(); // new instance of tool
+        // first destination is diamond for it will always be the most expensive at the beginning of round
+        destination = tool.nearestTile(TileType.RESOURCE_DIAMOND, startingBoard, startTileLocation);
+    }
+
+    /**
+     * Method that returns what action my robot will do on this round.
+     * Called at the beginning of every turn.
+     *
+     * @param boardView A PlayerBoardView object representing all the information about the board and the other player
+     *                   that your strategy is allowed to access
+     * @param economy The GameEngine's economy object which holds current prices for resources
+     * @param currentCharge The amount of charge your robot has (number of tile moves before needing to recharge)
+     * @param isRedTurn For use when two players attempt to move to the same spot on the same turn
+     *                   If true: The red player will move to the spot, and the blue player will do nothing
+     *                   If false: The blue player will move to the spot, and the red player will do nothing
+     * @return action as a TurnAction
+     */
+    @Override
+    public TurnAction getTurnAction(PlayerBoardView boardView, Economy economy, int currentCharge,
+            boolean isRedTurn) {
+        // current game board
+        Tool tool = new Tool(); // an instance of my Tool helper class
+        currentLocation = boardView.getYourLocation(); // sets current location to player location on board
+        TileType mostExpensiveTile = tool.itemToTile(tool.mostExpensiveResource(economy, boardView)); // updated most expensive tile type
+
+        // robot shouldn't leave charging station until fully charged
+        if (currentCharge < maxCharge && boardView.getTileTypeAtLocation(currentLocation).equals(TileType.RECHARGE)) {
+            return null;
+        }
+        // if robot needs to recharge
+        if (currentCharge <= maxCharge * 0.25 && currentCharge <= maxCharge) {
+            destination = tool.nearestTile(TileType.RECHARGE, boardView, currentLocation);
+            return tool.moveToDestination(currentLocation, destination);
+        } else {
+            if (currentInventorySize < maxInventorySize) { // mines most expensive resource until full inventory
+                if (currentLocation.x != destination.x || currentLocation.y != destination.y) {
+                    return tool.moveToDestination(currentLocation, destination);
+                }
+                if (mineCount < tool.minesRequired(mostExpensiveTile)) {
+                    mineCount++;
+                    return TurnAction.MINE;
+                }
+                destination = tool.nearestTile(mostExpensiveTile, boardView, currentLocation);
+                mineCount = 0;
+                return TurnAction.PICK_UP_RESOURCE;
+            } else if (currentInventorySize == maxInventorySize) { // goes to market when inventory is full
+                destination = tool.nearestTile(TileType.RED_MARKET, boardView, currentLocation);
+
+                if (currentLocation.x != destination.x || currentLocation.y != destination.y) {
+                    return tool.moveToDestination(currentLocation, destination);
+                }
+                destination = tool.nearestTile(mostExpensiveTile, boardView, currentLocation);
+                return tool.moveToDestination(currentLocation, destination);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Method that gets called on every time an item is picked up.
+     *  Inventory size gets increased by one every time method is called.
+     *
+     * @param itemReceived The item received from the player's TurnAction on their last turn
+     */
+    @Override
+    public void onReceiveItem(InventoryItem itemReceived) {
+        currentInventorySize++;
+    }
+
+    /**
+     * Method that gets called on every time robot steps on market and sells items in inventory.
+     *  Clears out current inventory size and adds points earned to an int current score tracker.
+     *
+     * @param totalSellPrice The combined sell price for all items in your strategy's inventory
+     */
+    @Override
+    public void onSoldInventory(int totalSellPrice) {
+        currentScore += totalSellPrice;
+        currentInventorySize = 0;
+    }
+
+    /**
+     * Method that returns the name of strategy.
+     *
+     * @return name of strategy
+     */
+    @Override
+    public String getName() {
+        return "I Tried";
+    }
+
+    /**
+     * Method called at the end of round to reset.
+     *
+     * @param pointsScored The total number of points this strategy scored
+     * @param opponentPointsScored The total number of points the opponent's strategy scored
+     */
+    @Override
+    public void endRound(int pointsScored, int opponentPointsScored) {
+    }
+}
---
+++ src/main/java/mineopoly_three/competition/Tool.java
@@ -0,0 +1,147 @@
+package mineopoly_three.competition;
+
+import java.awt.Point;
+import mineopoly_three.action.TurnAction;
+import mineopoly_three.game.Economy;
+import mineopoly_three.item.ItemType;
+import mineopoly_three.strategy.PlayerBoardView;
+import mineopoly_three.strategy.PlayerStrategy;
+import mineopoly_three.tiles.TileType;
+import mineopoly_three.util.DistanceUtil;
+
+/**
+ * Helper class that contains functions PlayerStrategy calls on to run game.
+ */
+public class Tool {
+    mineopoly_three.strategy.PlayerStrategy playerStrategy = new mineopoly_three.strategy.PlayerStrategy(); // an instance of PlayerStrategy
+
+    /**
+     * Helper method to determine the most expensive resource in economy during game.
+     *
+     * @param board current game board setup
+     * @return most expensive resource as a ItemType
+     */
+    public ItemType mostExpensiveResource(Economy economy, PlayerBoardView board) {
+        if (economy == null || board == null) {
+            return null;
+        }
+        ItemType mostExpensiveTile = null;
+        int maxValue = 0;
+        for (ItemType item: economy.getCurrentPrices().keySet()) {
+            if (economy.getCurrentPrices().get(item) > maxValue) {
+                maxValue = economy.getCurrentPrices().get(item);
+                mostExpensiveTile = item;
+            }
+        }
+        return mostExpensiveTile;
+    }
+
+    /**
+     * Helper method to convert an item to a tile.
+     *
+     * @param itemType to be converted to tile
+     * @return tile converted from item
+     */
+    public TileType itemToTile(ItemType itemType) {
+        if (itemType == null) {
+            return null;
+        }
+        TileType tileToReturn = TileType.EMPTY;
+        if (itemType.equals(ItemType.RUBY)) {
+            tileToReturn = TileType.RESOURCE_RUBY;
+        }
+        if (itemType.equals(ItemType.EMERALD)) {
+            tileToReturn = TileType.RESOURCE_EMERALD;
+        }
+        if (itemType.equals(ItemType.DIAMOND)) {
+            tileToReturn = TileType.RESOURCE_DIAMOND;
+        }
+        return tileToReturn;
+    }
+
+    /**
+     * Helper method that calculates the closest tile to go to.
+     *
+     * @param tile type wanted to reach
+     * @return location to go where the tile is at
+     */
+    public Point nearestTile(TileType tile, PlayerBoardView currentBoard, Point currentLocation) {
+        Point destination = new Point(Integer.MAX_VALUE/3, Integer.MAX_VALUE/3); // destination that bot goes toward
+
+        if (tile == null) {
+            return null;
+        }
+        for (int row = 0; row < mineopoly_three.strategy.PlayerStrategy.getCurrentBoardSize(); row++) {
+            for (int col = 0; col < mineopoly_three.strategy.PlayerStrategy.getCurrentBoardSize(); col++) {
+                if (currentBoard.getTileTypeAtLocation(row, col).equals(tile)) {
+                    Point temp = new Point(row, col);
+                    if (DistanceUtil.getManhattanDistance(temp, currentLocation) < DistanceUtil.getManhattanDistance(destination, currentLocation)) {
+                        destination = temp;
+                    }
+                }
+            }
+        }
+        return destination;
+    }
+
+    /**
+     * Helper method to determine the number of mine times needed.
+     *
+     * @return number of times to mine as an int
+     */
+    public int minesRequired(TileType tile) {
+        if (tile.equals(TileType.RESOURCE_RUBY)) {
+            return 1;
+        } else if (tile.equals(TileType.RESOURCE_EMERALD)) {
+            return 2;
+        } else if (tile.equals(TileType.RESOURCE_DIAMOND)) {
+            return 3;
+        }
+        return 0;
+    }
+
+    /**
+     * Helper method to move robot to destination desired from current location.
+     *
+     * @param currentLocation current location of player
+     * @param destination destination wanting to reach
+     * @return actions performed by robot
+     */
+    public TurnAction moveToDestination(Point currentLocation, Point destination) {
+        if (currentLocation == null || destination == null) {
+            return null;
+        }
+        if (currentLocation.x < destination.x) {
+            return TurnAction.MOVE_RIGHT;
+        } else if (currentLocation.x > destination.x) {
+            return TurnAction.MOVE_LEFT;
+        } else if (currentLocation.y < destination.y) {
+            return TurnAction.MOVE_UP;
+        } else if (currentLocation.y > destination.y) {
+            return TurnAction.MOVE_DOWN;
+        }
+        return null;
+    }
+
+    /**
+     * Helper method to set destination as points for robot to go to.
+     *
+     * @param tile to get to
+     * @param currentBoard current game board
+     * @return destination as point
+     */
+    public Point setDestination(TileType tile, PlayerBoardView currentBoard) {
+        if (tile == null || currentBoard == null) {
+            return null;
+        }
+        Point destination = playerStrategy.getDestination();
+        for (int row = 0; row < mineopoly_three.strategy.PlayerStrategy.getCurrentBoardSize(); row++) {
+            for (int col = 0; col < PlayerStrategy.getCurrentBoardSize(); col++) {
+                if (currentBoard.getTileTypeAtLocation(row, col).equals(tile)) {
+                    destination = new Point (row, col);
+                }
+            }
+        }
+        return destination;
+    }
+}
